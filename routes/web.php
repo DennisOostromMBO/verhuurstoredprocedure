af3a2681f2631c1174af3056ee4fd59d71ec62ab
Dennis Oostrom <336914@student.mboutrecht.nl>
2025-02-12 16:01:11 +0100

    opdracht klaar

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HallController;
+
+Route::get('/halls', [HallController::class, 'getHallsWithFacilities']);
 
 Route::get('/', function () {
     return view('welcome');
